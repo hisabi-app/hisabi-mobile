55f209ba9683001d6a00758f2154c3a5a3309f20
Ahmed Elsherif <ahmedelsherif.osama@gmail.com>
2024-04-03 01:36:34 +0400

    Activated Label visibility for unselected items, within bottom navigation bar
    Activated Label visibility for unselected items, within bottom navigation bar

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -4,18 +4,25 @@ class HomePage extends StatelessWidget {
   final navigationBarItems = const [
     BottomNavigationBarItem(label: "Dashbord", icon: Icon(Icons.dashboard)),
     BottomNavigationBarItem(
-        label: "Transactions  ", icon: Icon(Icons.transfer_within_a_station)),
+        label: "Transactions", icon: Icon(Icons.currency_exchange)),
     BottomNavigationBarItem(
         label: "Brands", icon: Icon(Icons.branding_watermark)),
     BottomNavigationBarItem(label: "Categories", icon: Icon(Icons.category)),
-    BottomNavigationBarItem(label: "SMS Parser", icon: Icon(Icons.translate)),
+    BottomNavigationBarItem(label: "Parser", icon: Icon(Icons.translate)),
   ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: navigationBarItems),
-      appBar: AppBar(),
-      body: const Text("Home Page"),
+      bottomNavigationBar: BottomNavigationBar(
+        items: navigationBarItems,
+        showUnselectedLabels: true,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.blueGrey,
+        onTap: (value) {
+          print(value);
+        },
+      ),
+      body: Center(child: const Text("Home Page")),
     );
   }
 }
