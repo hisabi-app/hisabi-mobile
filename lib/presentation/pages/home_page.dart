fa598e0ee9db9a37e290df4763543c4d20b86cee
Ahmed Elsherif <ahmedelsherif.osama@gmail.com>
2024-04-03 01:21:58 +0400

    Added Temp AppBar with mentioned items
    Added Temp AppBar with mentioned items

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final navigationBarItems = const [
+    BottomNavigationBarItem(label: "Dashbord", icon: Icon(Icons.dashboard)),
+    BottomNavigationBarItem(
+        label: "Transactions  ", icon: Icon(Icons.transfer_within_a_station)),
+    BottomNavigationBarItem(
+        label: "Brands", icon: Icon(Icons.branding_watermark)),
+    BottomNavigationBarItem(label: "Categories", icon: Icon(Icons.category)),
+    BottomNavigationBarItem(label: "SMS Parser", icon: Icon(Icons.translate)),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Text("Home Page");
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(items: navigationBarItems),
+      appBar: AppBar(),
+      body: const Text("Home Page"),
+    );
   }
 }
