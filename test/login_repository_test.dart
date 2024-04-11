7ecab51c47c592fec085f425760a9e7bc8a3650d
Ahmed Elsherif <ahmedelsherif.osama@gmail.com>
2024-04-12 02:27:24 +0400

    Updated Login with API, to call /api/login and return the token, then added a unit test to verify
    Updated Login with API, to call /api/login and return the token, then added a unit test to verify

---
+++ test/login_repository_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:cashify_mobile_flutter/domain/login_repository.dart';
+
+void main() {
+  group('LoginWithApiRepo', () {
+    test('login', () async {
+      // Arrange
+      final repo = LoginWithApiRepo();
+
+      // Act
+      final loginToken = await repo.login("admin@test.com", "admin123");
+
+      // Assert
+      expect(loginToken, isNotEmpty); // Assert that loginToken is not empty
+    });
+
+    test('login with invalid credentials', () async {
+      // Arrange
+      final repo = LoginWithApiRepo();
+
+      // Act
+      final loginToken =
+          await repo.login("invalid@example.com", "invalidpassword");
+
+      // Assert
+      expect(loginToken,
+          isEmpty); // Assert that loginToken is empty (since the login failed)
+    });
+  });
+}
