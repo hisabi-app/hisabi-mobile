41f1c162e66cf06b8fbb211565ab15894e6aa8b4
Ahmed Elsherif <ahmedelsherif.osama@gmail.com>
2024-05-14 08:05:53 +0400

    Created placeholder Text Widgets on Dashboard page
    Created placeholder Text Widgets on Dashboard page

--- lib/presentation/pages/dashboard_page.dart
+++ lib/presentation/pages/dashboard_page.dart
@@ -5,8 +5,48 @@ class DashboardPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final route = MaterialPageRoute(builder: (context) => DashboardPage());
 
-    return const Center(
-      child: Text("Dashboard"),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Text(
+                    "Account Overview",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  Text(
+                    "Networth",
+                  ),
+                  Text("Total Cash"),
+                  Text("Total Investment"),
+                  Text("Total Income"),
+                  Text("Total Expenses"),
+                  Text("Total Income Trend"),
+                  Text("Total Expenses Trend"),
+                  Text(
+                    "Categories Analytics",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  Text(
+                    "Brands Analytics",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  Text(
+                    "Facts",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  Text(
+                    "Visualization",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
