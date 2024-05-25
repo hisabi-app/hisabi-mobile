56377fae92d64349386378c166f8634522e9957c
Ahmed Elsherif <ahmedelsherif.osama@gmail.com>
2024-05-25 04:30:54 +0400

    Fixed search bar functionality, transactionsPage
    Fixed search bar functionality, transactionsPage

--- lib/presentation/pages/transactions_page.dart
+++ lib/presentation/pages/transactions_page.dart
@@ -11,171 +11,198 @@ class TransactionsPage extends StatefulWidget {
 class _TransactionsPageState extends State<TransactionsPage> {
   final route = MaterialPageRoute(builder: (context) => TransactionsPage());
 
-  final allTransactionsRepo = AllTransactionsRepo();
-  TextEditingController searchController = TextEditingController();
+  final filteredTransactionsRepo = FilteredTransactionsRepo();
+  SearchController _searchController = SearchController();
+  String? _token;
+  Future<dynamic>? _transactions;
 
   @override
   void initState() {
     super.initState();
+    _token = "";
+    _searchController.addListener(queryListener);
   }
 
-  void queryListener() {}
+  @override
+  void dispose() {
+    super.dispose();
+    _searchController.removeListener(queryListener);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void queryListener() {
+    setState(() {
+      _transactions = filteredTransactionsRepo.getTransactions(
+          _token!, _searchController.text);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final token = context.read<AppCubit>().state.token;
-    final transactions = allTransactionsRepo.getTransactions(token, "");
+    setState(() {
+      _token = token;
+    });
+
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
         child: SingleChildScrollView(
           child: Center(
-            child: FutureBuilder(
-                future: transactions,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(height: height * 0.3),
-                        const Center(child: CircularProgressIndicator()),
-                      ],
-                    );
-                  } else if (snapshot.hasError) {
-                    return Text(snapshot.error.toString());
-                  } else {
-                    return Center(
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: height * 0.1,
-                          ),
-                          Row(children: [
-                            SizedBox(
-                              width: width * 0.1,
-                            ),
-                            SearchBar(
-                              leading: const Icon(Icons.search),
-                              constraints: BoxConstraints(
-                                maxWidth: width * 0.8,
-                                minHeight: height * 0.05,
-                              ),
-                            ),
-                          ]),
-                          SizedBox(
-                            width: width * 0.1,
-                          ),
-                          SizedBox(
-                            height: height * 0.1,
-                          ),
-                          Row(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: height * 0.1,
+                ),
+                Row(children: [
+                  SizedBox(
+                    width: width * 0.1,
+                  ),
+                  SearchBar(
+                    controller: _searchController,
+                    leading: const Icon(Icons.search),
+                    constraints: BoxConstraints(
+                      maxWidth: width * 0.8,
+                      minHeight: height * 0.05,
+                    ),
+                  ),
+                ]),
+                SizedBox(
+                  width: width * 0.1,
+                ),
+                SizedBox(
+                  height: height * 0.1,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: width * 0.05,
+                    ),
+                    Container(width: width * 0.05, child: Text("ID")),
+                    SizedBox(
+                      width: width * 0.02,
+                    ),
+                    Container(
+                      width: width * 0.2,
+                      child: Text("AMOUNT"),
+                    ),
+                    SizedBox(
+                      width: width * 0.02,
+                    ),
+                    Container(
+                      width: width * 0.15,
+                      child: Text("BRAND"),
+                    ),
+                    SizedBox(
+                      width: width * 0.05,
+                    ),
+                    Container(
+                      width: width * 0.2,
+                      child: Text("CATEGORY"),
+                    ),
+                    SizedBox(
+                      width: width * 0.02,
+                    ),
+                    Container(
+                      width: width * 0.2,
+                      child: Text("DATE"),
+                    ),
+                    // SizedBox(
+                    //   width: width * 0.05,
+                    // ),
+                  ],
+                ),
+                FutureBuilder(
+                    future: _transactions,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(height: height * 0.3),
+                            const Center(child: CircularProgressIndicator()),
+                          ],
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text(snapshot.error.toString());
+                      } else {
+                        return Center(
+                          child: Column(
                             children: [
-                              SizedBox(
-                                width: width * 0.05,
-                              ),
-                              Container(width: width * 0.05, child: Text("ID")),
-                              SizedBox(
-                                width: width * 0.02,
-                              ),
                               Container(
-                                width: width * 0.2,
-                                child: Text("AMOUNT"),
+                                height: height * 0.45,
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: ListView.builder(
+                                        itemCount: snapshot.data?.length,
+                                        itemBuilder: (context, index) {
+                                          final data = snapshot.data;
+                                          final transaction = data![index];
+                                          return Center(
+                                            child: Row(
+                                              children: [
+                                                SizedBox(
+                                                  width: width * 0.05,
+                                                ),
+                                                Container(
+                                                    width: width * 0.07,
+                                                    child:
+                                                        Text(transaction.id)),
+                                                Container(
+                                                  margin:
+                                                      EdgeInsets.only(left: 0),
+                                                  width: width * 0.15,
+                                                  child: Text(transaction.amount
+                                                      .toString()),
+                                                ),
+                                                Container(
+                                                  width: width * 0.25,
+                                                  child: Text(transaction
+                                                      .brand.brandName),
+                                                ),
+                                                SizedBox(
+                                                  width: width * 0.02,
+                                                ),
+                                                Container(
+                                                  width: width * 0.17,
+                                                  child: Text(transaction
+                                                      .brand.category.name),
+                                                ),
+                                                SizedBox(
+                                                  width: width * 0.05,
+                                                ),
+                                                Container(
+                                                  width: width * 0.19,
+                                                  child: Text(
+                                                    transaction.date
+                                                        .toString()
+                                                        .substring(0, 10),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                              SizedBox(
-                                width: width * 0.02,
-                              ),
-                              Container(
-                                width: width * 0.15,
-                                child: Text("BRAND"),
-                              ),
-                              SizedBox(
-                                width: width * 0.05,
-                              ),
-                              Container(
-                                width: width * 0.2,
-                                child: Text("CATEGORY"),
-                              ),
-                              SizedBox(
-                                width: width * 0.02,
-                              ),
-                              Container(
-                                width: width * 0.2,
-                                child: Text("DATE"),
-                              ),
-                              // SizedBox(
-                              //   width: width * 0.05,
-                              // ),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: const Text("CREATE TRANSACTION")),
                             ],
                           ),
-                          Container(
-                            height: height * 0.45,
-                            child: Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Expanded(
-                                  child: ListView.builder(
-                                    itemCount: snapshot.data?.length,
-                                    itemBuilder: (context, index) {
-                                      final data = snapshot.data;
-                                      final transaction = data![index];
-                                      return Center(
-                                        child: Row(
-                                          children: [
-                                            SizedBox(
-                                              width: width * 0.05,
-                                            ),
-                                            Container(
-                                                width: width * 0.07,
-                                                child: Text(transaction.id)),
-                                            Container(
-                                              margin: EdgeInsets.only(left: 0),
-                                              width: width * 0.15,
-                                              child: Text(transaction.amount
-                                                  .toString()),
-                                            ),
-                                            Container(
-                                              width: width * 0.25,
-                                              child: Text(
-                                                  transaction.brand.brandName),
-                                            ),
-                                            SizedBox(
-                                              width: width * 0.02,
-                                            ),
-                                            Container(
-                                              width: width * 0.17,
-                                              child: Text(transaction
-                                                  .brand.category.name),
-                                            ),
-                                            SizedBox(
-                                              width: width * 0.05,
-                                            ),
-                                            Container(
-                                              width: width * 0.19,
-                                              child: Text(
-                                                transaction.date
-                                                    .toString()
-                                                    .substring(0, 10),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          TextButton(
-                              onPressed: () {},
-                              child: const Text("CREATE TRANSACTION")),
-                        ],
-                      ),
-                    );
-                  }
-                }),
+                        );
+                      }
+                    }),
+              ],
+            ),
           ),
         ),
       ),
