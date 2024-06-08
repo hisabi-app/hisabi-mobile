d9b5bcb57ff05c5c836977d2b667b414992f80bd
Ahmed Elsherif <ahmedelsherif.osama@gmail.com>
2024-06-08 21:43:02 +0400

    Replicated the Brand Page UI style to Category Page
    Replicated the Brand Page UI style to Category Page

--- lib/presentation/pages/categories_page.dart
+++ lib/presentation/pages/categories_page.dart
@@ -97,7 +97,7 @@ class _CategoriesPageState extends State<CategoriesPage> {
                           child: Column(
                             children: [
                               Container(
-                                height: height * 0.2,
+                                height: height * 0.45,
                                 child: Row(
                                   mainAxisSize: MainAxisSize.max,
                                   children: [
@@ -110,59 +110,75 @@ class _CategoriesPageState extends State<CategoriesPage> {
                                           return Center(
                                             child: Container(
                                               width: width * 0.7,
-                                              height: height * 0.2,
+                                              padding: EdgeInsetsDirectional
+                                                  .symmetric(
+                                                      vertical: height * 0.015),
+                                              margin: EdgeInsetsDirectional
+                                                  .symmetric(
+                                                      vertical: height * 0.010),
                                               decoration: BoxDecoration(
                                                   borderRadius:
                                                       BorderRadius.circular(5),
                                                   color: Colors.grey[100]),
-                                              child: Column(
+                                              child: Row(
                                                 children: [
-                                                  Row(
-                                                    mainAxisAlignment:
-                                                        MainAxisAlignment.start,
-                                                    children: [
-                                                      SizedBox(
-                                                        width: width * 0.33,
-                                                        height: height * 0.03,
-                                                      ),
-                                                      Container(
-                                                        height: height * 0.03,
-                                                        child: Text(
-                                                          category.id,
-                                                          style: TextStyle(
-                                                              fontWeight:
-                                                                  FontWeight
-                                                                      .w400,
-                                                              fontSize: 12),
-                                                        ),
-                                                      ),
-                                                    ],
+                                                  SizedBox(
+                                                    width: width * 0.03,
                                                   ),
                                                   Container(
-                                                    height: height * 0.035,
-                                                    child: Text(
-                                                      category.name
-                                                          .toUpperCase(),
-                                                      style: TextStyle(
-                                                          fontSize: 15,
-                                                          fontWeight:
-                                                              FontWeight.bold),
-                                                    ),
-                                                  ),
-                                                  Row(
-                                                    children: [
-                                                      SizedBox(
-                                                        width: width * 0.335,
-                                                      ),
-                                                      Container(
-                                                        height: height * 0.03,
-                                                        child: Text(
+                                                    width: width * 0.2,
+                                                    child: Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        Text(
+                                                          "${category.name}",
+                                                          style: TextStyle(
+                                                              fontSize: 15),
+                                                        ),
+                                                        Text(
                                                           category.type,
                                                           style: TextStyle(
+                                                              color: Color
+                                                                  .fromARGB(
+                                                                      255,
+                                                                      22,
+                                                                      53,
+                                                                      207)),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                  SizedBox(
+                                                    width: width * 0.2,
+                                                  ),
+                                                  Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      Container(
+                                                        child: Text(
+                                                          "Category ID",
+                                                          style: TextStyle(
+                                                            fontSize: 9,
+                                                            fontWeight:
+                                                                FontWeight.w400,
+                                                          ),
+                                                        ),
+                                                      ),
+                                                      Container(
+                                                        margin: EdgeInsets.only(
+                                                            left: 0),
+                                                        width: width * 0.20,
+                                                        child: Text(
+                                                          "${category.id}",
+                                                          style: TextStyle(
+                                                              fontSize: 15,
                                                               fontWeight:
                                                                   FontWeight
-                                                                      .w400,
-                                                              fontSize: 12),
+                                                                      .bold),
                                                         ),
                                                       ),
                                                     ],
